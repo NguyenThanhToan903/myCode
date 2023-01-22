8850af8787083b14456ad112643776c3a823abf4
NguyenThanhToanforWork <codehub.persoonal@gmail.com>
2023-01-22 13:57:29 +0700

    first

---
+++ code_java/code_eclipse/buoi1/src/buoi1/Cau2.java
@@ -0,0 +1,6 @@
+package buoi1;
+
+public class Cau2 {
+	public static void main(String[] args) {
+		System.out.println("Hello Java");	}
+}
--- code_java/test/src/test/maintest.java
+++
@@ -1,7 +0,0 @@
-package test;
-
-public class maintest {
-	public static void main(String a[]) {
-		System.out.print("chao Han ngoo ngok");
-	}
-}
